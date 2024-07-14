f6680e1b18f3408d584ca2f23131128983a7ab3d
Luiza <108764279+aziulll@users.noreply.github.com>
2024-07-14 19:37:40 -0300

    fix: add gitignore

--- src/utils/url.js
+++ src/utils/url.js
@@ -2,7 +2,13 @@
 const URL = [
     "https://www.netshoes.com.br/p/tenis-adidas-breaknet-feminino-branco+preto-NQQ-4379-890",
     "https://www.netshoes.com.br/p/tenis-under-armour-charged-first-preto+branco-39W-3305-172",
-    "https://www.netshoes.com.br/p/camisa-flamengo-i-2425-sn-torcedor-adidas-feminina-vermelho+preto-FB9-4228-068"
+    "https://www.netshoes.com.br/p/camisa-flamengo-i-2425-sn-torcedor-adidas-feminina-vermelho+preto-FB9-4228-068",
+    "https://www.netshoes.com.br/p/camisa-flamengo-i-2425-sn-torcedor-adidas-feminina-vermelho+preto-FB9-4228-068",
+    "https://www.netshoes.com.br/p/kit-4-camisetas-dry-alpha-co-masculina-QJA-0068-351?sellerId=13235&promoted=true",
+    "https://www.netshoes.com.br/p/camiseta-umbro-x-panini-flag-soccer-brasil-masculina-2IA-9373-030",
+    "https://www.netshoes.com.br/p/camisa-nike-brasil-i-202425-torcedor-pro-masculina-JD8-6585-030",
+    "https://www.netshoes.com.br/p/bone-new-era-mlb-new-york-yankees-940-ny-preto-BRJ-0513-006",
+    "https://www.netshoes.com.br/p/best-whey-iso-protein-atlhetica-nutrition-900g-baunilha-097-9208-963"
   ];
   
   module.exports = URL;
